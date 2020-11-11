a2bfc0603c50d289e23563263bf38d01b99751b8
MEianelli <filipelouzano@gmail.com>
2020-11-11 18:13:50 -0300

    1 ao 15 feitos

--- script.js
+++ script.js
@@ -21,3 +21,35 @@ function gerarCarta() {
     paragrafo.appendChild(spanText);
   });
 }
+
+function formarClasseAleatoria() {
+  var estilo = ["newspaper", "magazine1", "magazine2"];
+  var tamanho = ["medium", "big", "reallybig"];
+  var rotacao = ["rotateleft", "rotateright"];
+  var inclinacao = ["skewleft", "skewright"];
+
+  let de0a31 = Math.floor(Math.random() * 4);
+  let de0a32 = Math.floor(Math.random() * 4);
+  let de0a21 = Math.floor(Math.random() * 3);
+  let de0a22 = Math.floor(Math.random() * 3);
+
+  let classe = "";
+
+  if (estilo[de0a31] !== undefined) {
+    classe = classe.concat(" ", estilo[de0a31]);
+  }
+
+  if (tamanho[de0a32] !== undefined) {
+    classe = classe.concat(" ", tamanho[de0a32]);
+  }
+
+  if (rotacao[de0a21] !== undefined) {
+    classe = classe.concat(" ", rotacao[de0a21]);
+  }
+
+  if (inclinacao[de0a22] !== undefined) {
+    classe = classe.concat(" ", inclinacao[de0a22]);
+  }
+
+  return classe;
+}
