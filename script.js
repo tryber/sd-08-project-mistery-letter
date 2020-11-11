bf4bf5f577e4adffc2ca25561a8e0bcbbbdeed9b
Luiz Reina <luizreeina@gmail.com>
2020-11-11 17:20:38 -0300

    requisitos 1 a 3

---
+++ script.js
@@ -0,0 +1,45 @@
+const input = document.getElementById('carta-texto');
+const btnCria = document.getElementById('criar-carta');
+const cartaGerada = document.getElementById('carta-gerada');
+let isEmpty = true;
+
+
+function checaInput() {
+  for (let index = 0; index < (input.value).length; index += 1){
+    if (input.value[index] !== ' '){
+      isEmpty = false;
+      return;
+    }
+  }
+}
+
+function limpaCarta() {
+  const spanList = document.querySelectorAll('span');
+
+  for (let index = 0; index < spanList.length; index += 1) {
+    spanList[index].remove();
+  }
+}
+
+function criaCarta() {
+  limpaCarta();
+  checaInput();
+
+  if (isEmpty === true) {
+    alert('Por favor, digite o conteúdo da carta.');
+    return;
+  }
+
+  let words = (input.value).split(' ');
+
+  for (let index = 0; index < words.length; index += 1) {
+    const span = document.createElement('span');
+    span.innerText = words[index];
+    cartaGerada.append(span);
+  }
+
+  isEmpty = true;
+}
+
+btnCria.addEventListener('click', criaCarta);
+
