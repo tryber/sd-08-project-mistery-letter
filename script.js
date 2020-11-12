4b0c00579ed1560f948d6ed2d8ae1abb20bd6313
Lucas Gama <lucasbg@protonmail.com>
2020-11-12 08:51:54 -0300

    req6 a 16: adiciona as classes aleatorias em cada palavra

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ btnCriarCarta.addEventListener('click', function () {
   for (let index = 0; index < cartaVect.length; index += 1) {
     const word = document.createElement('span');
     word.innerText = cartaVect[index];
+    word.classList.add(...getRandClasses());
     paragrafo.appendChild(word);
     paragrafo.innerHTML = paragrafo.innerHTML + ' ';
   }
@@ -16,3 +17,35 @@ btnCriarCarta.addEventListener('click', function () {
     paragrafo.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 });
+
+const estilos = [`newspaper`, `magazine1`, `magazine2`];
+const tamanhos = [`medium`, `big`, `reallybig`];
+const rotacoes = [`rotateleft`, `rotateright`];
+const inclinacoes = [`skewleft`, `skewright`];
+
+function allProperties() {
+  return [estilos, tamanhos, rotacoes, inclinacoes];
+}
+const minClasses = 2;
+
+function getRandClasses() {
+  const selectedProp = [];
+  const selectedClasses = [];
+  const properties = allProperties();
+  const numberOfClasses = Math.floor(
+    minClasses + Math.random() * (properties.length - minClasses + 1)
+  );
+
+  for (let index = 0; index < numberOfClasses; index += 1) {
+    const selectPos = Math.floor(Math.random() * properties.length);
+    selectedProp.push(properties[selectPos]);
+    properties.splice(selectPos, 1);
+  }
+
+  for (let index = 0; index < selectedProp.length; index += 1) {
+    const selectPos = Math.floor(Math.random() * selectedProp[index].length);
+    selectedClasses.push(selectedProp[index][selectPos]);
+  }
+
+  return selectedClasses;
+}
