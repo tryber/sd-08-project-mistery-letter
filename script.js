db8b2cefb39691a1a114a95d063a867c5344d19d
Lucas Gama <lucasbg@protonmail.com>
2020-11-12 07:59:30 -0300

    req5: verifica se o conteudo digitado é vazio

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const paragrafo = document.getElementById('carta-gerada');
 
 btnCriarCarta.addEventListener('click', function () {
   const carta = campoTexto.value;
+  paragrafo.innerHTML = '';
   const cartaVect = carta.split(' ');
   for (let index = 0; index < cartaVect.length; index += 1) {
     const word = document.createElement('span');
     word.innerText = cartaVect[index];
     paragrafo.appendChild(word);
     paragrafo.innerHTML = paragrafo.innerHTML + ' ';
+  }
+  if (paragrafo.innerText == '') {
+    paragrafo.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 });
