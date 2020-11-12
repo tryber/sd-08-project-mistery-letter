7b1adc36ccd06d86e1b54ea52709f14554f52238
Lucas Rodrigues <lucas.movimento@gmail.com>
2020-11-12 12:39:18 -0300

    acertos requisitos 03, 05 e 16

--- script.js
+++ script.js
@@ -1,76 +1,82 @@
 const letterBtn = document.getElementById('criar-carta');
-const generatedLetter = document.getElementById('carta-gerada');
-
-const classesGroups = {
-  estilo: [`newspaper`, `magazine1`, `magazine2`],
-  tamanho: [`medium`, `big`, `reallybig`],
-  rotacao: [`rotateleft`, `rotateright`, 'none'],
-  inclinacao: [`skewleft`, `skewright`, 'none']
-}
-
 
 function splitText(str) {
-  return str.split(' ');
+    return str.split(' ');
 }
 
-function createSpan(word) {
-  const spanText = document.createElement('span');
-  spanText.className = 'palavra';
-  spanText.innerHTML = word;
-  generatedLetter.appendChild(spanText);
-}
+function randomClasses() {
+  const estilo = [`newspaper`, `magazine1`, `magazine2`];
+  const tamanho = [`medium`, `big`, `reallybig`];
+  const rotacao = [`rotateleft`, `rotateright`, 'none'];
+  const inclinacao = [`skewleft`, `skewright`, 'none'];
 
-function randomIndexClasses() {
-  const arrayWords = generatedLetter.children;
+  let classes = '';
 
-  for (let indexSpan = 0; indexSpan < arrayWords.length; indexSpan += 1) {
-    for (let indexGrp = 1; indexGrp <= 4; indexGrp += 1) {
-      let classIndex = Math.round(Math.random() * 3);
-      if (classIndex < 0) {
-        classIndex = 0;
-      } else if (classIndex >= 3) {
-        classIndex = 2
-      }
-  
-      switch (indexGrp) {
-        case 1:
-          arrayWords[indexSpan].classList.add(classesGroups.estilo[classIndex]);
-          break;
-        case 2:
-          arrayWords[indexSpan].classList.add(classesGroups.tamanho[classIndex]);
-          break;
-        case 3:
-          arrayWords[indexSpan].classList.add(classesGroups.rotacao[classIndex]);
-          break;
-        case 4:
-          arrayWords[indexSpan].classList.add(classesGroups.inclinacao[classIndex]);
-          break;
-        default:
-          console.log('error on the random!');
-      }
+  for (let indexGrp = 1; indexGrp <= 4; indexGrp += 1) {
+    let classIndex = Math.round(Math.random() * 3);
+    if (classIndex < 0) {
+      classIndex = 0;
+    } else if (classIndex >= 3) {
+      classIndex = 2;
     }
 
+    switch (indexGrp) {
+      case 1:
+        classes = classes.concat(" ", estilo[classIndex]);
+        break;
+      case 2:
+        classes = classes.concat(" ", tamanho[classIndex]);
+        break;
+      case 3:
+        if (rotacao[classIndex] != 'none') {
+          classes = classes.concat(" ", rotacao[classIndex]);
+        }
+        break;
+      case 4:
+        if (inclinacao[classIndex] != 'none') {
+          classes = classes.concat(" ", inclinacao[classIndex]);
+        }
+        break;
+      default:
+        console.log('error on the random!');
+    }
   }
+  return classes;
 }
 
-letterBtn.addEventListener('click', function () {
+letterBtn.addEventListener('click', createLetter);
+
+function createLetter() {
+  const outputLetter = document.getElementById('output');
+  const letterParagraph = document.createElement('p');
+  letterParagraph.className = 'carta-gerada';
+
+  outputLetter.appendChild(letterParagraph);
+
   const inputText = document.getElementById('carta-texto').value;
   const arrayText = splitText(inputText);
+
   let emptyArray = false;
 
   for (let i = 0; i < arrayText.length; i += 1) {
     let word = arrayText[i];
+
     if (word != '') {
-      createSpan(word);
+      const spanText = document.createElement('span');
+      spanText.className = randomClasses();
+      spanText.innerHTML = word;
+      letterParagraph.appendChild(spanText);
     } else {
       emptyArray = true;
     }
   }
 
-  if(emptyArray) {
-    alert('Por favor, digite o conteúdo da carta.');
+  if (emptyArray) {
+    const spanText = document.createElement('span');
+    spanText.className = 'erro';
+    spanText.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    letterParagraph.appendChild(spanText);
     document.getElementById('carta-texto').value = '';
   }
-  
-  randomIndexClasses();
-});
+}
+
