65c873544e33e58e7e9f9b06d89f540028fd59f8
emassaki <massakihirayama@gmail.com>
2020-11-14 16:53:36 -0300

    Adicionado requisitos 4 e 5 - gerar uma mensagem se o input estiver em branco

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const botaoGerar = document.getElementById('criar-carta');
 const pCarta = document.getElementById('carta-gerada');
 
 function gerarCarta() {
+  if (/\s+$/.test(inputCarta.value)) alert('Por favor, digite o conteúdo da carta');
   const inputCartaArray = inputCarta.value.split(' ');
+  console.log(inputCartaArray);
   for (let palavra in inputCartaArray) {
+    if (inputCartaArray[palavra] !== '') { 
     const spanDaPalavra = document.createElement('span');
     spanDaPalavra.innerHTML = inputCartaArray[palavra];
     spanDaPalavra.classList.add('palavra');
     pCarta.appendChild(spanDaPalavra);
+    }
   }
 }
-botaoGerar.addEventListener('click', gerarCarta)
+
+botaoGerar.addEventListener('click', gerarCarta);
+
