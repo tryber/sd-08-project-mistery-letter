50bcf5fdcb3306f56d2444932f240fba77e9987e
emassaki <massakihirayama@gmail.com>
2020-11-16 08:59:13 -0300

    Corrigindo requisitos

--- script.js
+++ script.js
@@ -14,22 +14,30 @@ const pContador = document.getElementById('carta-contador');
 //   'skewleft',
 //   'skewright',
 // ];
-const grupoEstilos = ['newspaper', 'magazine1', 'magazine2']; //classes.slice(0, 3);
-const grupoTamanho = ['medium', 'big', 'reallybig']; //classes.slice(3, 6);
-const grupoRotacao = ['rotateleft', 'rotateright']; //classes.slice(6, 8);
-const grupoInclinacao = ['skewleft', 'skewright']; //classes.slice(8, 10);
+const grupoEstilos = ['newspaper', 'magazine1', 'magazine2']; // classes.slice(0, 3);
+const grupoTamanho = ['medium', 'big', 'reallybig']; // classes.slice(3, 6);
+const grupoRotacao = ['rotateleft', 'rotateright']; // classes.slice(6, 8);
+const grupoInclinacao = ['skewleft', 'skewright']; // classes.slice(8, 10);
 const grupo = [grupoEstilos, grupoTamanho, grupoRotacao, grupoInclinacao];
 
 function estilosAleatorios(elemento) {
-  elemento.className = 'palavra';
+  elemento.removeAttribute('class');
   const classesAdicionadas = [];
-  for (let grupoDeEstilos = 0; grupoDeEstilos < grupo.length; grupoDeEstilos += 1) {
+  for (
+    let grupoDeEstilos = 0;
+    grupoDeEstilos < grupo.length;
+    grupoDeEstilos += 1
+  ) {
     const estilo = grupo[grupoDeEstilos];
-    const indexDoEstilo = Math.floor(Math.random() * grupo[grupoDeEstilos].length);
+    const indexDoEstilo = Math.floor(
+      Math.random() * grupo[grupoDeEstilos].length
+    );
     classesAdicionadas.push(estilo[indexDoEstilo]);
   }
   for (let classes = 0; classes < classesAdicionadas.length; classes += 1) {
-    const indexDaClasseAdicionada = Math.floor(Math.random() * classesAdicionadas.length);
+    const indexDaClasseAdicionada = Math.floor(
+      Math.random() * classesAdicionadas.length
+    );
     elemento.classList.add(classesAdicionadas[indexDaClasseAdicionada]);
   }
 }
@@ -46,18 +54,19 @@ function gerarCarta() {
     if (inputCartaArray[palavra] !== '') {
       const spanDaPalavra = document.createElement('span');
       spanDaPalavra.innerHTML = inputCartaArray[palavra];
-      spanDaPalavra.className = 'palavra';
+      spanDaPalavra.addEventListener('click', (event) => {estilosAleatorios(event.target)})
+      // spanDaPalavra.className = 'palavra';
       estilosAleatorios(spanDaPalavra);
       pCarta.appendChild(spanDaPalavra);
     }
   }
-  const numeroDePalavras = document.querySelectorAll('.palavra').length;
+  const numeroDePalavras = document.querySelectorAll('span').length;
   pContador.innerHTML = numeroDePalavras;
 }
 botaoGerar.addEventListener('click', gerarCarta);
 
-pCarta.addEventListener('click', (event) => {
-  if (event.target.classList.contains('palavra')) {
-    estilosAleatorios(event.target);
-  }
-});
+// pCarta.addEventListener('click', (event) => {
+//   if (event.target.classList.contains('palavra')) {
+//     estilosAleatorios(event.target);
+//   }
+// });
