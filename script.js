5a03d18dcc7177f60780435ad1a434475780dbb9
Ivanildo Cândido <systechtecnologia@gmail.com>
2020-11-22 21:52:58 -0300

    Finalizando Projeto

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 const createLetter = document.getElementById('criar-carta');
 const genLetter = document.getElementById('carta-gerada');
 const inputLetter = document.getElementById('carta-texto');
+const countDisplay = document.getElementById('carta-contador');
+function checkText() {
+  if (inputLetter.value === ' ' || inputLetter.value === '') {
+    genLetter.innerText = 'Por favor, digite o conteúdo da carta.';
+    return true;
+  }
+  return false;
+}
+function addClass(element) {
+  const classStyles = ['newspaper', 'magazine1', 'magazine2'];
+  const classLength = ['medium', 'big', 'reallybig'];
+  const classRotate = ['rotateleft', 'rotateright'];
+  const classSkew = ['skewleft', 'skewright'];
+  const forThree = Math.floor(Math.random() * 3);
+  const forTwo = Math.floor(Math.random() * 2);
+  element.classList.add(classStyles[forThree]);
+  element.classList.add(classLength[forThree]);
+  element.classList.add(classRotate[forTwo]);
+  element.classList.add(classSkew[forTwo]);
+}
+function modifyClasses() {
+  const spans = document.querySelectorAll('#carta-gerada span');
+  for (let index = 0; index < spans.length; index += 1) {
+    spans[index].addEventListener('click', function (event) {
+      event.target.classList = '';
+      addClass(event.target);
+    });
+  }
+}
 
 function generateLetter() {
-  createLetter.addEventListener('click', function () {
-    let listWords = [];
-    listWords = inputLetter.value.split(' ');
-    for (let index = 0; index < listWords.length; index += 1) {
-      const span = document.createElement('span');
-      span.innerText = listWords[index];
-      genLetter.appendChild(span);
-    }
-  });
+  if (checkText()) {
+    return;
+  }
+  let listWords = [];
+  listWords = inputLetter.value.split(' ');
+  countDisplay.innerHTML = listWords.length;
+  for (let index = 0; index < listWords.length; index += 1) {
+    const span = document.createElement('span');
+    span.style.display = 'inline-block';
+    span.innerText = listWords[index];
+    addClass(span);
+    genLetter.appendChild(span);
+  }
+  modifyClasses();
 }
-generateLetter();
+createLetter.addEventListener('click', generateLetter);
