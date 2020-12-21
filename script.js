54d086aacc302bf6e80836e7b0b0c24a04ac848f
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-12-21 16:14:37 -0300

    criação de classes no css

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const button = document.querySelector("#criar-carta");
 const paragraph = document.querySelector("#carta-gerada");
 const input = document.querySelector("#carta-texto");
 
+const removeAllChilds = (element) => {
+  while (element.firstChild) {
+    element.firstChild.remove();
+  }
+};
 const generateLetter = () => {
+  removeAllChilds(paragraph);
+  if (input.value === undefined || input.value.trim() === "") {
+    paragraph.innerHTML = "Por favor, digite o conteúdo da carta.";
+  }
   const wordsArray = input.value.split(" ");
   wordsArray.forEach((elem) => {
     const span = document.createElement("span");
     span.innerHTML = `${elem} `;
-    paragraph.appendChild(span);
+    if (span.innerHTML !== "") {
+      paragraph.appendChild(span);
+    }
   });
 };
-
 
 window.onload = () => {
   button.addEventListener("click", generateLetter);
