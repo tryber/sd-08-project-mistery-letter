8d3e3d0b81a34a31a56407e5f54012ee076dfdaf
Doug-Fernando <doug.fernando95@gmail.com>
2020-12-23 19:10:52 -0300

    corrigindo req 3

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const cartaGerada = document.querySelector('#carta-gerada');
 
 const gerarCarta = () => {
   if (!carta.value || !carta.value.trim()) {
-    alert('Por favor, digite o conteúdo da carta.');
+    cartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
   }
   const separarPalavras = carta.value.split(' ');
   separarPalavras.forEach((palavra) => {
     const novoSpan = document.createElement('span');
-    novoSpan.innerHTML = `${palavra} `;
+    novoSpan.innerHTML = `${palavra}`;
     cartaGerada.append(novoSpan);
   });
 };
