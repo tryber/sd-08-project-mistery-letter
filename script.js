c50019ccd5f49a869147c2a0d9f5d86702555eb5
Doug-Fernando <doug.fernando95@gmail.com>
2020-12-23 18:49:41 -0300

    req 3, 4 e 5

---
+++ script.js
@@ -0,0 +1,17 @@
+const carta = document.querySelector('#carta-texto');
+const criarCarta = document.querySelector('#criar-carta');
+const cartaGerada = document.querySelector('#carta-gerada');
+
+const gerarCarta = () => {
+  if (!carta.value || !carta.value.trim()) {
+    alert('Por favor, digite o conteúdo da carta.');
+  }
+  const separarPalavras = carta.value.split(' ');
+  separarPalavras.forEach((palavra) => {
+    const novoSpan = document.createElement('span');
+    novoSpan.innerHTML = palavra + ' ';
+    cartaGerada.append(novoSpan);
+  });
+};
+
+criarCarta.addEventListener('click', gerarCarta);
