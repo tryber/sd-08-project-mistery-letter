72c88836e117c2890b910aab059f7cd50390a705
Doug-Fernando <doug.fernando95@gmail.com>
2020-12-24 17:20:13 -0300

    Req final para concluir projeto

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 const carta = document.querySelector('#carta-texto');
 const criarCarta = document.querySelector('#criar-carta');
 const cartaGerada = document.querySelector('#carta-gerada');
-const gerarClasse = [
-  'newspaper',
-  'magazine1',
-  'magazine2',
-  'medium',
-  'big',
-  'reallybig',
-  'rotateleft',
-  'rotateright',
-  'skewleft',
-  'skewright',
-];
+const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
+const grupoTamanho = ['medium', 'big', 'reallybig'];
+const grupoRotacao = ['rotateleft', 'rotateright'];
+const grupoInclinacao = ['skewleft', 'skewright'];
 
 const gerarCarta = () => {
   cartaGerada.innerHTML = '';
@@ -23,8 +15,33 @@ const gerarCarta = () => {
   separarPalavras.forEach((palavra) => {
     const novoSpan = document.createElement('span');
     novoSpan.innerHTML = `${palavra}`;
+    novoSpan.classList.add('palavra');
+    adicionarClasses(novoSpan);
     cartaGerada.append(novoSpan);
   });
+};
+
+const adicionarClasses = (palavra) => {
+  const gerarEstilo = Math.floor(Math.random() * grupoEstilo.length);
+  const gerarTamanho = Math.floor(Math.random() * grupoTamanho.length);
+  const gerarRotacaoo = Math.floor(Math.random() * grupoRotacao.length);
+  const gerarInclinacao = Math.floor(Math.random() * grupoInclinacao.length);
+
+  palavra.classList.add(grupoEstilo[gerarEstilo]);
+  palavra.classList.add(grupoTamanho[gerarTamanho]);
+
+  const adicionarInclinacao = Math.floor(
+    Math.random() * grupoInclinacao.length
+  );
+  const adicionarRotacao = Math.floor(Math.random() * grupoRotacao.length);
+
+  if (adicionarInclinacao > 0.4) {
+    palavra.classList.add(grupoRotacao[gerarRotacaoo]);
+  }
+
+  if (adicionarRotacao > 0.5) {
+    palavra.classList.add(grupoInclinacao[gerarInclinacao]);
+  }
 };
 
 criarCarta.addEventListener('click', gerarCarta);
