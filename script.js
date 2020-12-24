e07c9be0ac733eb1df5eaf2d5e56848fb44c1635
Doug-Fernando <doug.fernando95@gmail.com>
2020-12-24 17:44:35 -0300

    Corrige req

--- script.js
+++ script.js
@@ -6,21 +6,6 @@ const grupoTamanho = ['medium', 'big', 'reallybig'];
 const grupoRotacao = ['rotateleft', 'rotateright'];
 const grupoInclinacao = ['skewleft', 'skewright'];
 
-const gerarCarta = () => {
-  cartaGerada.innerHTML = '';
-  if (!carta.value || !carta.value.trim()) {
-    cartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
-  }
-  const separarPalavras = carta.value.split(' ');
-  separarPalavras.forEach((palavra) => {
-    const novoSpan = document.createElement('span');
-    novoSpan.innerHTML = `${palavra}`;
-    novoSpan.classList.add('palavra');
-    adicionarClasses(novoSpan);
-    cartaGerada.append(novoSpan);
-  });
-};
-
 const adicionarClasses = (palavra) => {
   const gerarEstilo = Math.floor(Math.random() * grupoEstilo.length);
   const gerarTamanho = Math.floor(Math.random() * grupoTamanho.length);
@@ -30,18 +15,30 @@ const adicionarClasses = (palavra) => {
   palavra.classList.add(grupoEstilo[gerarEstilo]);
   palavra.classList.add(grupoTamanho[gerarTamanho]);
 
-  const adicionarInclinacao = Math.floor(
-    Math.random() * grupoInclinacao.length
-  );
-  const adicionarRotacao = Math.floor(Math.random() * grupoRotacao.length);
+  const addInclinacao = Math.floor(Math.random() * grupoInclinacao.length);
+  const addRotacao = Math.floor(Math.random() * grupoRotacao.length);
 
-  if (adicionarInclinacao > 0.4) {
+  if (addInclinacao > 0.4) {
     palavra.classList.add(grupoRotacao[gerarRotacaoo]);
   }
 
-  if (adicionarRotacao > 0.5) {
+  if (addRotacao > 0.5) {
     palavra.classList.add(grupoInclinacao[gerarInclinacao]);
   }
+};
+
+const gerarCarta = () => {
+  cartaGerada.innerHTML = '';
+  if (!carta.value || !carta.value.trim()) {
+    cartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+  const separarPalavras = carta.value.split(' ');
+  separarPalavras.forEach((palavra) => {
+    const novoSpan = document.createElement('span');
+    novoSpan.innerHTML = `${palavra}`;
+    adicionarClasses(novoSpan);
+    cartaGerada.append(novoSpan);
+  });
 };
 
 criarCarta.addEventListener('click', gerarCarta);
