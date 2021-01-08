8ab1de695a01dbfc6675ac6b2ae93992ea0ba2b3
Thiago Ferreira <thiagocristhian@gmail.com>
2021-01-08 12:36:09 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const addletter = () => {
   document.querySelector('#criar-carta').addEventListener('click', () => {
     const words = document.getElementById('carta-texto').value;
     const arrayWords = words.split(' ');
+    const cartaGerada = document.querySelector('#carta-gerada');
+    cartaGerada.innerHTML = '';
     arrayWords.map((element) => {
       const span = document.createElement('span');
-      span.innerHTML = element.concat('  ');
+      span.innerHTML = element;
       const insertSpan = document.querySelector('#carta-gerada');
       return insertSpan.appendChild(span);
     });
